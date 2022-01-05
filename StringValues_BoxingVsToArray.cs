133849bceb929eb837cad40efef4831f9479386f
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2022-01-05 10:41:51 -0500

    more benchmarks

---
+++ StringValues_BoxingVsToArray.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Microsoft.Extensions.Primitives;
+
+namespace Benchmarks;
+
+[SimpleJob(RuntimeMoniker.Net461)]
+[SimpleJob(RuntimeMoniker.NetCoreApp31)]
+//[SimpleJob(RuntimeMoniker.Net60)]
+[MemoryDiagnoser]
+[DisassemblyDiagnoser]
+public class StringValues_BoxingVsToArray
+{
+    private StringValues values = new("foo");
+    private IEnumerable<string> temp;
+
+    [Benchmark]
+    public void Box()
+    {
+        Foo(values);
+    }
+
+    [Benchmark]
+    public void ToArray()
+    {
+        Foo(values.ToArray());
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+    private void Foo(IEnumerable<string> values)
+    {
+    }
+}
---
+++ YieldVsNewArray.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace Benchmarks;
+
+[SimpleJob(RuntimeMoniker.Net461)]
+[SimpleJob(RuntimeMoniker.NetCoreApp31)]
+//[SimpleJob(RuntimeMoniker.Net60)]
+[MemoryDiagnoser]
+[DisassemblyDiagnoser]
+public class YieldVsNewArray
+{
+    [Benchmark]
+    public void Yield()
+    {
+        _ = YieldImpl("foo");
+    }
+
+    private IEnumerable<string> YieldImpl(string value)
+    {
+        yield return value;
+    }
+
+    [Benchmark]
+    public void NewArray()
+    {
+        _ = NewArrayImpl("foo");
+    }
+
+    private IEnumerable<string> NewArrayImpl(string value)
+    {
+        return new[] { value };
+    }
+}
