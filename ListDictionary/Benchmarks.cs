ec31980c5d3d2e686e3b3df6e5e6a354a647a3b4
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2024-11-11 13:55:24 -0500

    update ListDictionary benchmarks

--- ListDictionary/Benchmarks.cs
+++ ListDictionary/Benchmarks.cs
@@ -1,54 +1,147 @@
 ﻿using System.Collections.Generic;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
+using Datadog.Trace;
 
 namespace ListDictionary;
 
-[SimpleJob(RuntimeMoniker.Net461)]
+//[SimpleJob(RuntimeMoniker.Net461)]
 [SimpleJob(RuntimeMoniker.Net60)]
 [MemoryDiagnoser]
 public class Benchmarks
 {
-    [Params(5, 10, 30)]
+    [Params(1, 4, 8, 16, 32)]
     public int ItemCount { get; set; }
 
-    private List<KeyValuePair<string, string>> _listBaggage;
-    private Dictionary<string, string> _dictionaryBaggage;
-    private string[] _keys;
+    [Benchmark]
+    public void List()
+    {
+        var baggage1 = new Baggage();
+
+        // add items upstream
+        for (int i = 0; i < ItemCount; i++)
+        {
+            baggage1[$"k{i}"] = $"v{i}";
+        }
+
+        // iterate to send downstream
+        foreach (var pair in baggage1)
+        {
+            _ = pair.Value;
+            _ = pair.Key;
+        }
+
+        var baggage2 = new Baggage();
+
+        // add items downstream
+        foreach (var pair in baggage1)
+        {
+            baggage2[pair.Key] = pair.Value;
+        }
+
+        // lookup each item
+        for (int i = 0; i < ItemCount; i++)
+        {
+            _ = baggage2[$"k{i}"];
+        }
+    }
+
+    [Benchmark]
+    public void Dictionary()
+    {
+        var baggage1 = new Dictionary<string, string>();
+
+        // add items upstream
+        for (int i = 0; i < ItemCount; i++)
+        {
+            baggage1[$"k{i}"] = $"v{i}";
+        }
+
+        // iterate to send downstream
+        foreach (var pair in baggage1)
+        {
+            _ = pair.Value;
+            _ = pair.Key;
+        }
+
+        var baggage2 = new Dictionary<string, string>();
+
+        // add items downstream
+        foreach (var pair in baggage1)
+        {
+            baggage2[pair.Key] = pair.Value;
+        }
+
+        // lookup each item
+        for (int i = 0; i < ItemCount; i++)
+        {
+            _ = baggage2[$"k{i}"];
+        }
+    }
+
+    /*
+    private List<KeyValuePair<string, string>> _list;
+    private Dictionary<string, string> _dictionary;
     private string _existingKey;
 
     [GlobalSetup]
     public void Setup()
     {
-        _listBaggage = new List<KeyValuePair<string, string>>(ItemCount);
-        _dictionaryBaggage = new Dictionary<string, string>(ItemCount);
-        _keys = new string[ItemCount];
+        _list = new List<KeyValuePair<string, string>>(ItemCount);
+        _dictionary = new Dictionary<string, string>(ItemCount);
+        var keys = new string[ItemCount];
 
         for (int i = 0; i < ItemCount; i++)
         {
             var key = $"key{i}";
             var value = $"value{i}";
-            _keys[i] = key;
-            _listBaggage.Add(new KeyValuePair<string, string>(key, value));
-            _dictionaryBaggage[key] = value;
+            keys[i] = key;
+            _list.Add(new KeyValuePair<string, string>(key, value));
+            _dictionary[key] = value;
         }
 
         // Select an existing key to test lookup performance
-        _existingKey = _keys[ItemCount / 2];
+        _existingKey = keys[ItemCount * 2 / 3];
+    }
+
+    [Benchmark]
+    public List<KeyValuePair<string, string>> Add_List()
+    {
+        var list = new List<KeyValuePair<string, string>>();
+
+        for (int i = 0; i < ItemCount; i++)
+        {
+            list.Add(new KeyValuePair<string, string>($"key{i}", $"value{i}"));
+        }
+
+        return list;
+    }
+
+    [Benchmark]
+    public Dictionary<string, string> Add_Dictionary()
+    {
+        var dictionary = new Dictionary<string, string>();
+
+        for (int i = 0; i < ItemCount; i++)
+        {
+            dictionary[$"key{i}"] = $"value{i}";
+        }
+
+        return dictionary;
     }
 
     [Benchmark]
     public void AddOrReplace_List()
     {
-        var key = "newKey";
-        var value = "newValue";
+        const string key = "newKey";
+        const string value = "newValue";
         bool found = false;
 
-        for (int i = 0; i < _listBaggage.Count; i++)
+        for (int i = 0; i < _list.Count; i++)
         {
-            if (_listBaggage[i].Key == key)
+            if (_list[i].Key == key)
             {
-                _listBaggage[i] = new KeyValuePair<string, string>(key, value);
+                _list[i] = new KeyValuePair<string, string>(key, value);
                 found = true;
                 break;
             }
@@ -56,16 +149,14 @@ public class Benchmarks
 
         if (!found)
         {
-            _listBaggage.Add(new KeyValuePair<string, string>(key, value));
+            _list.Add(new KeyValuePair<string, string>(key, value));
         }
     }
 
     [Benchmark]
     public void AddOrReplace_Dictionary()
     {
-        var key = "newKey";
-        var value = "newValue";
-        _dictionaryBaggage[key] = value;
+        _dictionary["newKey"] = "newValue";
     }
 
     [Benchmark]
@@ -73,7 +164,7 @@ public class Benchmarks
     {
         string temp = null;
 
-        foreach (var item in _listBaggage)
+        foreach (var item in _list)
         {
             temp = item.Key;
         }
@@ -86,7 +177,7 @@ public class Benchmarks
     {
         string temp = null;
 
-        foreach (var kvp in _dictionaryBaggage)
+        foreach (var kvp in _dictionary)
         {
             temp = kvp.Key;
         }
@@ -97,7 +188,7 @@ public class Benchmarks
     [Benchmark]
     public string Lookup_List()
     {
-        foreach (var item in _listBaggage)
+        foreach (var item in _list)
         {
             if (item.Key == _existingKey)
             {
@@ -111,18 +202,18 @@ public class Benchmarks
     [Benchmark]
     public string Lookup_Dictionary()
     {
-        _dictionaryBaggage.TryGetValue(_existingKey, out var temp);
+        _dictionary.TryGetValue(_existingKey, out var temp);
         return temp;
     }
 
     [Benchmark]
     public void Remove_List()
     {
-        for (int i = 0; i < _listBaggage.Count; i++)
+        for (int i = 0; i < _list.Count; i++)
         {
-            if (_listBaggage[i].Key == _existingKey)
+            if (_list[i].Key == _existingKey)
             {
-                _listBaggage.RemoveAt(i);
+                _list.RemoveAt(i);
                 break;
             }
         }
@@ -131,6 +222,7 @@ public class Benchmarks
     [Benchmark]
     public void Remove_Dictionary()
     {
-        _dictionaryBaggage.Remove(_existingKey);
+        _dictionary.Remove(_existingKey);
     }
+    */
 }
